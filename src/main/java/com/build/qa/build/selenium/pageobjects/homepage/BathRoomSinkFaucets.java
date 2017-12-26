1ff53622e3a52828ea22f9a9e4439d81bbfb4357
spamudur <spamudur@LNAR-PC036SKV.corp.capgemini.com>
2017-12-25 23:31:29 -0500

    COMMITING

---
+++ src/main/java/com/build/qa/build/selenium/pageobjects/homepage/BathRoomSinkFaucets.java
@@ -0,0 +1,24 @@
+package com.build.qa.build.selenium.pageobjects.homepage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+
+import com.build.qa.build.selenium.pageobjects.BasePage;
+
+public class BathRoomSinkFaucets extends BasePage {
+
+private By buildThemeBody;
+	
+	public BathRoomSinkFaucets(WebDriver driver, Wait<WebDriver> wait) {
+		super(driver, wait);
+		buildThemeBody = By.cssSelector("body.build-theme");
+	}
+	
+	public boolean onBuildTheme() { 
+		return wait.until(ExpectedConditions.presenceOfElementLocated(buildThemeBody)) != null;
+	}
+
+
+}
